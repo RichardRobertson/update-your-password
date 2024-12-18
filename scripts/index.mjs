162bb75ad2078153c27ffc708f024bd03bc199d2
Richard Robertson <richard@rarobertson.net>
2024-12-18 10:48:25 -0600

    Manual merge of pull request #5
    Thanks to @esheyw for these changes. Had to manually make the changes to resolve style conflicts.

--- scripts/index.mjs
+++ scripts/index.mjs
@@ -1,22 +1,39 @@
-"use strict";
+const MODULE_ID = "update-your-password";
 
 function updatePasswordDialog() {
     new Dialog({
         title: game.i18n.localize("update-your-password.dialog.title"),
-        content: `<label for="new-password">${game.i18n.localize("update-your-password.dialog.label.contents")}</label><input id="new-password" type="password"><label for="confirm-new-password">${game.i18n.localize("update-your-password.dialog.confirm-label.contents")}</label><input id="confirm-new-password" type="password">`,
+        content: `
+        <form>
+            <div class="form-group">
+                <label for="${MODULE_ID}-new-password">
+                    ${game.i18n.localize("update-your-password.dialog.label.contents")}
+                </label>
+                <div class="form-fields">
+                    <input id="${MODULE_ID}-new-password" type="password" name="newPassword">
+                </div>
+            </div>
+            <div class="form-group">
+                <label for="${MODULE_ID}-confirm-new-password">
+                    ${game.i18n.localize("update-your-password.dialog.confirm-label.contents")}
+                </label>
+                <div class="form-fields">
+                    <input id="${MODULE_ID}-confirm-new-password" type="password" name="confirmNewPassword">
+                </div>
+            </div>
+        </form>`,
         buttons: {
             apply: {
-                icon: '<i class="fas fa-check"></i>',
+                icon: '<i class="fa-solid fa-check"></i>',
                 label: game.i18n.localize("update-your-password.dialog.buttons.apply.label"),
                 callback: async (html) => {
-                    const newPassword = html.find("#new-password")[0].value;
-                    const confirmPassword = html.find("#confirm-new-password")[0].value;
-                    if (newPassword !== confirmPassword) {
-                        ui.notifications.error(
+                    const fde = new FormDataExtended(html[0].querySelector("form"));
+                    const { newPassword, confirmNewPassword } = fde.object;
+                    if (newPassword !== confirmNewPassword) {
+                        return ui.notifications.error(
                             "update-your-password.notifications.error.confirm-not-match",
                             { localize: true }
                         );
-                        return;
                     }
                     if (newPassword.length !== 0) {
                         await game.user.update({ password: newPassword });
@@ -24,7 +41,7 @@ function updatePasswordDialog() {
                 },
             },
             cancel: {
-                icon: '<i class="fas fa-times"></i>',
+                icon: '<i class="fa-solid fa-times"></i>',
                 label: game.i18n.localize("update-your-password.dialog.buttons.cancel.label"),
             },
         },
@@ -33,7 +50,7 @@ function updatePasswordDialog() {
 }
 
 Hooks.on("init", function () {
-    game.settings.register("update-your-password", "show-token-tool", {
+    game.settings.register(MODULE_ID, "show-token-tool", {
         name: "update-your-password.settings.show-token-tool.name",
         hint: "update-your-password.settings.show-token-tool.hint",
         scope: "world",
@@ -42,7 +59,7 @@ Hooks.on("init", function () {
         default: false,
         requiresReload: true,
     });
-    game.settings.register("update-your-password", "show-user-context-menu", {
+    game.settings.register(MODULE_ID, "show-user-context-menu", {
         name: "update-your-password.settings.show-user-context-menu.name",
         hint: "update-your-password.settings.show-user-context-menu.hint",
         scope: "world",
@@ -51,7 +68,7 @@ Hooks.on("init", function () {
         default: true,
         requiresReload: true,
     });
-    game.settings.register("update-your-password", "expose-dialog-function", {
+    game.settings.register(MODULE_ID, "expose-dialog-function", {
         name: "update-your-password.settings.expose-dialog-function.name",
         hint: "update-your-password.settings.expose-dialog-function.hint",
         scope: "world",
@@ -63,11 +80,11 @@ Hooks.on("init", function () {
 });
 
 Hooks.on("getSceneControlButtons", function (controls) {
-    if (game.settings.get("update-your-password", "show-token-tool")) {
+    if (game.settings.get(MODULE_ID, "show-token-tool")) {
         let tokenControls = controls.find((x) => x.name === "token");
         tokenControls.tools.push({
-            icon: "fas fa-key",
-            name: "update-your-password",
+            icon: "fa-solid fa-key",
+            name: MODULE_ID,
             title: game.i18n.localize("update-your-password.tool.title"),
             button: true,
             onClick: updatePasswordDialog,
@@ -78,16 +95,20 @@ Hooks.on("getSceneControlButtons", function (controls) {
 Hooks.on("getUserContextOptions", function (html, contextOptions) {
     contextOptions.push({
         name: game.i18n.localize("update-your-password.tool.title"),
-        icon: '<i class="fas fa-key"></i>',
+        icon: '<i class="fa-solid fa-key"></i>',
         callback: updatePasswordDialog,
-        condition: (html) =>
-            html.attr("data-user-id") === game.userId &&
-            game.settings.get("update-your-password", "show-user-context-menu"),
+        condition: (li) => {
+            li = li instanceof HTMLElement ? li : li[0];
+            return (
+                li.dataset.userId === game.userId &&
+                game.settings.get(MODULE_ID, "show-user-context-menu")
+            );
+        },
     });
 });
 
 Hooks.on("ready", function () {
-    if (game.settings.get("update-your-password", "expose-dialog-function")) {
+    if (game.settings.get(MODULE_ID, "expose-dialog-function")) {
         game.updateYourPassword = Object.freeze({
             showDialog: updatePasswordDialog,
         });
